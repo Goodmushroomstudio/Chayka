a3d55d437106fed3591b91ee575c82fbd5842af3
goodmushroomstudio <exotarium@mail.ru>
2017-09-02 13:58:51 +0200

    k

--- Chayka/Assets/Joystick.cs
+++ Chayka/Assets/Joystick.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Joystick : MonoBehaviour {
-    
+    float currentPosition, deltaPositon, lastPositon;
+
+
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        currentPosition = Input.mousePosition.y;
+        deltaPositon = currentPosition - lastPositon;
+        lastPositon = currentPosition;
         if (Input.GetMouseButton(0))
         {
-            
-            transform.position = new Vector3(transform.position.x, Input.mousePosition.y, transform.position.z);
-            transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Clamp(transform.localPosition.y, -240, 240), transform.localPosition.z);
+            if (deltaPositon != 0)
+            {
+                transform.position -= new Vector3(0, deltaPositon, 0);
+                transform.localPosition = new Vector3(transform.localPosition.x, Mathf.Clamp(transform.localPosition.y, -240, 240), transform.localPosition.z);
+            }
         }
+        GameData.gd.f_axisY = transform.localPosition.y / 240;
 	}
     
     public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z)
