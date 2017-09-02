db0a85158529d289a86f402dbf3fab0445b5f1ed
DESKTOP-8LC8SFF\Valek <exotarium@mail.ru>
2017-09-02 13:45:36 +0300

    v

---
+++ Chayka/Assets/Anim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Anim : MonoBehaviour {
+    int i_currentFrame;
+    float f_time;
+    public float f_maxTime;
+    public Sprite[] spriteArray;
+	// Use this for initialization
+	void Start () {
+        i_currentFrame = 0;
+        GetComponent<SpriteRenderer>().sprite = spriteArray[i_currentFrame];
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        f_time -= 1 * Time.deltaTime;
+        f_time = Mathf.Clamp(f_time, 0, f_maxTime);
+        if(f_time==0)
+        {
+            i_currentFrame++;
+            GetComponent<SpriteRenderer>().sprite = spriteArray[i_currentFrame];
+            f_time = f_maxTime;
+            if (i_currentFrame + 1 > spriteArray.Length-1)
+            {
+                i_currentFrame = 0;
+            }
+        }
+        
+	}
+}
