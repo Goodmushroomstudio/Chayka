3279960b4bfb57fcd63994c3fd13c1bbf7ff55c1
DESKTOP-8LC8SFF\Valek <exotarium@mail.ru>
2017-09-07 21:03:44 +0300

    v

---
+++ Chayka/Assets/Mob.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob : MonoBehaviour {
+    public float f_reload;
+    public float f_timer;
+
+	// Use this for initialization
+	void Start () {
+        f_timer = f_reload;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        f_timer -= 1 * Time.deltaTime;
+        if (f_timer <= 0)
+        {
+            transform.GetChild(0).GetComponent<Animation>().Play("Boom");
+            f_timer = f_reload;
+        }
+		
+	}
+}
