8189c7906f34c0acf3ab3fa0478707d97b1e8a12
goodmushroomstudio <exotarium@mail.ru>
2017-09-26 17:47:37 +0200

    k

--- Chayka/Assets/Scripts/Water.cs
+++ Chayka/Assets/Scripts/Water.cs
@@ -4,51 +4,23 @@ using UnityEngine;
 
 public class Water : MonoBehaviour {
 
-    public int layer;
     public float speed;
-    float min, max;
-    bool up;
-    public bool move;
 	// Use this for initialization
 	void Start () {
-        min = transform.position.y - 0.2f;
-        max = transform.position.y + 0.2f;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        if (move)
-        {
-            transform.position -= new Vector3(speed * GameData.gd.f_speed, 0, 0) * Time.deltaTime;
+    void Update()
+    {
 
-            if (transform.position.y < max && up)
-            {
-                transform.position += new Vector3(0, speed / 5, 0) * Time.deltaTime;
-                if (transform.position.y > max)
-                    up = false;
-            }
-            else if (transform.position.y > min && !up)
-            {
-                transform.position -= new Vector3(0, speed / 5, 0) * Time.deltaTime;
-                if (transform.position.y < min)
-                    up = true;
-            }
-        }
-        if (!move)
+        transform.position -= new Vector3(speed * GameData.gd.f_speed, 0, 0) * Time.deltaTime;
+
+
+        if (transform.position.x < Camera.main.ScreenToWorldPoint(Vector3.zero).x - 13)
         {
-            if (transform.position.x < Camera.main.ScreenToWorldPoint(Vector3.zero).x - 13)
-            {
-                transform.position += new Vector3(57.6f, 0, 0);
-            }
+            transform.position += new Vector3(GetComponent<SpriteRenderer>().bounds.size.x*9, 0, 0);
         }
-        else
-        {
-            if (transform.position.x < Camera.main.ScreenToWorldPoint(Vector3.zero).x - 13)
-            {
-                transform.localPosition += new Vector3(57.6f, 0, 0);
-                transform.GetChild(0).position -= new Vector3(57.6f, 0, 0);
-                transform.GetChild(1).position -= new Vector3(57.6f, 0, 0);
-            }
-        }
-	}
+
+    }
 }
