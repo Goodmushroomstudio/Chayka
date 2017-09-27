9150c906d28f34dcffe5eac8a8f600f375462d6c
goodmushroomstudio <exotarium@mail.ru>
2017-09-27 21:21:33 +0200

    k

--- Chayka/Assets/Scripts/Water.cs
+++ Chayka/Assets/Scripts/Water.cs
@@ -19,17 +19,18 @@ public class Water : MonoBehaviour {
         transform.position -= new Vector3(speed * GameData.gd.f_speed, 0, 0) * Time.deltaTime;
 
 
-        if (transform.position.x < Camera.main.ScreenToWorldPoint(Vector3.zero).x - 5)
+        if (transform.GetChild(0).position.x < Camera.main.ScreenToWorldPoint(Vector3.zero).x - 5)
         {
-            transform.position += new Vector3(GetComponent<SpriteRenderer>().bounds.size.x*11, 0, 0);
+            transform.GetChild(0).position += new Vector3(transform.GetChild(0).GetComponent<SpriteRenderer>().bounds.size.x * 11, 0, 0);
+            transform.GetChild(0).SetAsLastSibling();
         }
         if (generateBubble)
         {
             if (transform.position.x <= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0)).x)
             {
-                if (Random.Range(0, 100) == 0)
+                if (Random.Range(0, 20) == 0)
                 {
-                    Instantiate(bubble, new Vector3(Random.Range(GetComponent<SpriteRenderer>().bounds.min.x, GetComponent<SpriteRenderer>().bounds.max.x), Camera.main.ScreenToWorldPoint(Vector3.zero).y - 1), Quaternion.identity, transform);
+                    Instantiate(bubble, new Vector3(Random.Range(transform.GetChild(0).GetComponent<SpriteRenderer>().bounds.min.x, Mathf.Abs(transform.GetChild(transform.childCount-1).GetComponent<SpriteRenderer>().bounds.max.x)), Camera.main.ScreenToWorldPoint(Vector3.zero).y - 1), Quaternion.identity, transform);
                 }
             }
         }
